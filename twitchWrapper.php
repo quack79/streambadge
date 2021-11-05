70deb58f0af730a60bb10e778f0d0fcd00b6dc1b
quack79 <30216196+quack79@users.noreply.github.com>
2021-11-05 16:01:45 -0400

    Update twitchWrapper.php

--- twitchWrapper.php
+++ twitchWrapper.php
@@ -4,29 +4,62 @@ namespace TwitchApiClass;
 
 class twitchWrapper
 {
-    private const CLIENT_ID = '9mkmqucsg7qqo1o76d7ldc7rx1igsa';
-    private const CLIENT_SECRET = 'REMOVED';
-    private const ACCESS_CODE = 'REMOVED'; // Authorization code
-    private const REDIRECT_URI = 'https://quack.me/streambadge/'; // Your applications redirect URI
-    private const TOKEN_FILENAME = 'accesstoken.txt'; // Change this name. It is the file your token will be stored in
+    private const CLIENT_ID = 'REDACTED'; 
+    private const CLIENT_SECRET = 'REDACTED';
+    private const TOKEN_FILENAME = 'accesstoken.txt'; // This is where your token will be stored
 
     private const URI = 'https://api.twitch.tv'; // DO NOT CHANGE
     private const TWITCH_OUATH_URI = 'https://id.twitch.tv/oauth2'; // DO NOT CHANGE
 
     private string $access_token = '';
-    private string $refresh_token = '';
     private string $expires_dt = '';
-    private array $call_data;
     private array $stream_data = [];
 
     public function __construct()
     {
-        $this->getTokensFromFile();
+        $this->getTokenFromFile();
     }
 
-    public function accessCodeUrl(string $scope = "user:edit+user:read:email"): string
+    public function getTokenFromFile(): void
     {
-        return self::TWITCH_OUATH_URI . "/authorize?response_type=code&client_id=" . self::CLIENT_ID . "&redirect_uri=" . self::REDIRECT_URI . "&scope=$scope";
+        if (!file_exists(self::TOKEN_FILENAME)) { // Create the file if it doesn't exist
+            $this->refreshToken();
+        }
+        $tokens = json_decode(file_get_contents(self::TOKEN_FILENAME), false);
+        $this->access_token = $tokens->access_token;
+        $this->expires_dt = $tokens->expires;
+        if ($this->dateTimePassed($this->expires_dt)) {
+            $this->refreshToken();
+            $this->getTokensFromFile();
+        }
+    }
+
+    public function refreshToken(): bool
+    {
+		$url = self::TWITCH_OUATH_URI . "/token?client_id=" . self::CLIENT_ID . "&client_secret=" . self::CLIENT_SECRET . "&grant_type=client_credentials";
+		$data = $this->doCurl($url, "POST");
+        $contents = '{"access_token": "' . $data['access_token'] . '", "expires": "' . $this->addSecondsToDateTime($data['expires_in']) . '"}';
+        $fp = fopen(self::TOKEN_FILENAME, 'wb');
+        fwrite($fp, $contents);
+        return fclose($fp);
+    }
+
+    private function GETHeaders(): array
+    {
+        return array("Authorization: Bearer $this->access_token", "Client-ID: " . self::CLIENT_ID);
+    }
+
+    public function getUserStream(string $username): array
+    {
+        return $this->stream_data = $this->doCurl(self::URI . '/helix/streams?user_login=' . rawurlencode($username), 'GET', $this->GETHeaders());
+    }
+
+    public function userIsLive(): ?bool
+    {
+        if (isset($this->stream_data['data'][0])) {
+            return true; // Yes
+        }
+        return false; // No
     }
 
     public function doCurl(string $url, string $type = 'GET', array $headers = [], array $post_fields = [], int $con_timeout = 5, int $timeout = 20): array
@@ -55,7 +88,7 @@ class twitchWrapper
         if ($responseInfo['http_code'] === 200) {
             return json_decode($call_response, true);
         }
-        return array('http_response_code' => $responseInfo['http_code']);// Call failed
+        return array('http_response_code' => $responseInfo['http_code']); // Call failed
     }
 
     public function dateTimePassed(string $dt_to_check): bool
@@ -71,58 +104,4 @@ class twitchWrapper
         date_default_timezone_set('UTC');
         return date("Y-m-d H:i:s", strtotime("+{$seconds} sec"));
     }
-
-    public function getTokensFromFile(): void
-    {
-        if (!file_exists(self::TOKEN_FILENAME)) { // First time use = create the file
-            $this->createTokenFile();
-        }
-        $tokens = json_decode(file_get_contents(self::TOKEN_FILENAME), false);
-        $this->access_token = $tokens->access_token;
-        $this->refresh_token = $tokens->refresh_token;
-        $this->expires_dt = $tokens->expires;
-        if ($this->dateTimePassed($this->expires_dt)) {
-            $this->refreshToken();
-            $this->getTokensFromFile();
-        }
-    }
-
-    public function createTokenFile(): bool
-    {
-        $url = self::TWITCH_OUATH_URI . "/token?client_id=" . self::CLIENT_ID . "&client_secret=" . self::CLIENT_SECRET . "&code=" . self::ACCESS_CODE . "&grant_type=authorization_code&redirect_uri=" . self::REDIRECT_URI;
-        $data = $this->doCurl($url, "POST");
-        $contents = '{"access_token": "' . $data['access_token'] . '", "refresh_token": "' . $data['refresh_token'] . '", "expires": "' . $this->addSecondsToDateTime($data['expires_in']) . '"}';
-        $fp = fopen(self::TOKEN_FILENAME, 'wb');
-        fwrite($fp, $contents);
-        return fclose($fp);
-    }
-
-    public function refreshToken(): bool
-    {
-        $url = self::TWITCH_OUATH_URI . "/token?grant_type=refresh_token&refresh_token=$this->refresh_token&client_id=" . self::CLIENT_ID . "&client_secret=" . self::CLIENT_SECRET;
-        $data = $this->doCurl($url, 'POST');
-        $contents = '{"access_token": "' . $data['access_token'] . '", "refresh_token": "' . $data['refresh_token'] . '"}';
-        $fp = fopen(self::TOKEN_FILENAME, 'wb');
-        fwrite($fp, $contents);
-        return fclose($fp);
-    }
-
-    private function GETHeaders(): array
-    {
-        return array("Authorization: Bearer $this->access_token", "Client-ID: " . self::CLIENT_ID);
-    }
-
-    public function getUserStream(string $username): array
-    {
-        return $this->stream_data = $this->doCurl(self::URI . '/helix/streams?user_login=' . rawurlencode($username), 'GET', $this->GETHeaders());
-    }
-
-    public function userIsLive(): ?bool
-    {
-        if (isset($this->stream_data['data'][0])) {
-            return true; // Yes
-        }
-        return false; // No
-    }
-
 }
